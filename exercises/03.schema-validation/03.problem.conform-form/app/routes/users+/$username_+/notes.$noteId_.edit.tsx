3236f9cd24a336720d9a3d81d05601f78b1cdd6a
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-05-06 19:04:38 -0400

    refactor: streamline form handling in NoteEdit component by removing unnecessary hooks and integrating conform for field management

--- exercises/03.schema-validation/03.problem.conform-form/app/routes/users+/$username_+/notes.$noteId_.edit.tsx
+++ exercises/03.schema-validation/03.problem.conform-form/app/routes/users+/$username_+/notes.$noteId_.edit.tsx
@@ -1,4 +1,5 @@
-import { parse } from '@conform-to/zod'
+import { conform, useForm } from '@conform-to/react'
+import { getFieldsetConstraint, parse } from '@conform-to/zod'
 import {
 	json,
 	redirect,
@@ -6,7 +7,6 @@ import {
 	type LoaderFunctionArgs,
 } from '@remix-run/node'
 import { Form, useActionData, useLoaderData } from '@remix-run/react'
-import { useEffect, useRef, useState } from 'react'
 import { z } from 'zod'
 import { GeneralErrorBoundary } from '#app/components/error-boundary.tsx'
 import { floatingToolbarClassName } from '#app/components/floating-toolbar.tsx'
@@ -16,11 +16,7 @@ import { Label } from '#app/components/ui/label.tsx'
 import { StatusButton } from '#app/components/ui/status-button.tsx'
 import { Textarea } from '#app/components/ui/textarea.tsx'
 import { db, updateNote } from '#app/utils/db.server.ts'
-import {
-	invariantResponse,
-	useFocusInvalid,
-	useIsSubmitting,
-} from '#app/utils/misc.tsx'
+import { invariantResponse, useIsSubmitting } from '#app/utils/misc.tsx'
 
 export async function loader({ params }: LoaderFunctionArgs) {
 	const note = db.note.findFirst({
@@ -82,119 +78,61 @@ function ErrorList({
 	) : null
 }
 
-// 💣 You can delete this hook. Conform will handle this for us.
-function useHydrated() {
-	const [hydrated, setHydrated] = useState(false)
-	useEffect(() => setHydrated(true), [])
-	return hydrated
-}
-
 export default function NoteEdit() {
 	const data = useLoaderData<typeof loader>()
 	const actionData = useActionData<typeof action>()
-	// 💣 we don't need the formRef anymore
-	const formRef = useRef<HTMLFormElement>(null)
-	// 💣 we don't need the formId anymore
-	const formId = 'note-editor'
 	const isSubmitting = useIsSubmitting()
 
-	// 💣 delete everything between here and the next 💣
-	const fieldErrors =
-		actionData?.status === 'error' ? actionData.submission.error : null
-	const formErrors =
-		actionData?.status === 'error' ? actionData.submission.error[''] : null
-	const isHydrated = useHydrated()
-
-	const formHasErrors = Boolean(formErrors?.length)
-	const formErrorId = formHasErrors ? 'form-error' : undefined
-	const titleHasErrors = Boolean(fieldErrors?.title?.length)
-	const titleErrorId = titleHasErrors ? 'title-error' : undefined
-	const contentHasErrors = Boolean(fieldErrors?.content?.length)
-	const contentErrorId = contentHasErrors ? 'content-error' : undefined
-
-	useFocusInvalid(
-		formRef.current,
-		actionData?.status === 'error' && !isSubmitting,
-	)
-	// 💣 delete everything between here and the previous 💣
-	// Conform does a lot for us huh!? 🤯
-
-	// 🐨 add your useForm config here
-	// 💰 reference the instructions for what it should look like
-	// 💰 make sure to check on the defaultValue in the instructions as well
+	const [form, fields] = useForm({
+		id: 'note-editor',
+		constraint: getFieldsetConstraint(NoteEditorSchema),
+		lastSubmission: actionData?.submission,
+		onValidate({ formData }) {
+			return parse(formData, { schema: NoteEditorSchema })
+		},
+		defaultValue: {
+			title: data.note.title,
+			content: data.note.content,
+		},
+	})
 
 	return (
 		<div className="absolute inset-0">
 			<Form
-				// 💣 delete the id and noValidate props
-				id={formId}
-				noValidate={isHydrated}
 				method="post"
 				className="flex h-full flex-col gap-y-4 overflow-y-auto overflow-x-hidden px-10 pb-28 pt-12"
-				// 💣 delete the rest of these props
-				aria-invalid={formHasErrors || undefined}
-				aria-describedby={formErrorId}
-				ref={formRef}
-				tabIndex={-1}
-				// 🐨 add {...form.props} here
+				{...form.props}
 			>
 				<div className="flex flex-col gap-1">
 					<div>
-						{/* 🐨 replace the hard-coded "note-title" for fields.title.id */}
-						<Label htmlFor="note-title">Title</Label>
-						<Input
-							// 💣 everything between here and the next 💣 can be deleted
-							id="note-title"
-							name="title"
-							defaultValue={data.note.title}
-							required
-							maxLength={titleMaxLength}
-							aria-invalid={titleHasErrors || undefined}
-							aria-describedby={titleErrorId}
-							// 💣 everything between here and the previous 💣 can be deleted
-							autoFocus
-							// 🐨 add {...conform.input(fields.title)} here
-						/>
+						<Label htmlFor={fields.title.id}>Title</Label>
+						<Input autoFocus {...conform.input(fields.title)} />
 						<div className="min-h-[32px] px-4 pb-3 pt-1">
-							{/* 🐨 get the id from fields.title.errorId */}
-							{/* 🐨 get the errors from fields.title.errors */}
-							<ErrorList id={titleErrorId} errors={fieldErrors?.title} />
+							<ErrorList
+								id={fields.title.errorId}
+								errors={fields.title.errors}
+							/>
 						</div>
 					</div>
 					<div>
-						{/* 🐨 replace the hard-coded "note-content" for fields.content.id */}
-						<Label htmlFor="note-content">Content</Label>
-						<Textarea
-							// 💣 everything between here and the next 💣 can be deleted
-							id="note-content"
-							name="content"
-							defaultValue={data.note.content}
-							required
-							maxLength={contentMaxLength}
-							aria-invalid={contentHasErrors || undefined}
-							aria-describedby={contentErrorId}
-							// 💣 everything between here and the previous 💣 can be deleted
-							// 🐨 add {...conform.textarea(fields.content)} here
-						/>
+						<Label htmlFor={fields.content.id}>Content</Label>
+						<Textarea {...conform.textarea(fields.content)} />
 						<div className="min-h-[32px] px-4 pb-3 pt-1">
-							{/* 🐨 get the id from fields.content.errorId */}
-							{/* 🐨 get the errors from fields.content.errors */}
-							<ErrorList id={contentErrorId} errors={fieldErrors?.content} />
+							<ErrorList
+								id={fields.content.errorId}
+								errors={fields.content.errors}
+							/>
 						</div>
 					</div>
 				</div>
-				{/* 🐨 get the id from form.errorId */}
-				{/* 🐨 get the errors from form.errors */}
-				<ErrorList id={formErrorId} errors={formErrors} />
+				<ErrorList id={form.errorId} errors={form.errors} />
 			</Form>
 			<div className={floatingToolbarClassName}>
-				{/* 🐨 replace formId with form.id */}
-				<Button form={formId} variant="destructive" type="reset">
+				<Button form={form.id} variant="destructive" type="reset">
 					Reset
 				</Button>
 				<StatusButton
-					// 🐨 replace formId with form.id
-					form={formId}
+					form={form.id}
 					type="submit"
 					disabled={isSubmitting}
 					status={isSubmitting ? 'pending' : 'idle'}
