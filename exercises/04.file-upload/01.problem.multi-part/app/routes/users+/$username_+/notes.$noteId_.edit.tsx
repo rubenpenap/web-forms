0ce52f13b2504be70a9bcc686801db63c0b3da4e
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-05-07 11:43:43 -0400

    feat: implement multipart form data handling in NoteEdit component with image upload support

--- exercises/04.file-upload/01.problem.multi-part/app/routes/users+/$username_+/notes.$noteId_.edit.tsx
+++ exercises/04.file-upload/01.problem.multi-part/app/routes/users+/$username_+/notes.$noteId_.edit.tsx
@@ -5,6 +5,8 @@ import {
 	redirect,
 	type ActionFunctionArgs,
 	type LoaderFunctionArgs,
+	unstable_createMemoryUploadHandler as createMemoryUploadHandler,
+	unstable_parseMultipartFormData as parseMultipartFormData,
 } from '@remix-run/node'
 import { Form, useActionData, useLoaderData } from '@remix-run/react'
 import { useState } from 'react'
@@ -50,10 +52,13 @@ const NoteEditorSchema = z.object({
 export async function action({ request, params }: ActionFunctionArgs) {
 	invariantResponse(params.noteId, 'noteId param is required')
 
-	// 🐨 switch this for parseMultipartFormData and createMemoryUploadHandler
-	// 🐨 set the `maxPartSize` setting to 3MB (💰 1024 * 1024 * 3) to prevent
-	// 🐨 users from uploading files that are too large.
-	const formData = await request.formData()
+	const formData = await parseMultipartFormData(
+		request,
+		createMemoryUploadHandler({
+			maxPartSize: 1024 * 1024 * 3,
+		}),
+	)
+
 	const submission = parse(formData, {
 		schema: NoteEditorSchema,
 	})
@@ -69,12 +74,13 @@ export async function action({ request, params }: ActionFunctionArgs) {
 		id: params.noteId,
 		title,
 		content,
-		// 🐨 add an images array and pass an object that has the
-		// id, file, and altText that was submitted.
-		// Right now, we're just going to grab these values straight from formData.
-		// 💰 formData.get('file'), etc.
-		// In the next step, we'll add this to the Zod schema.
-		// 🦺 TypeScript won't like this. We'll fix it in the next step, so don't worry about it.
+		images: [
+			{
+				id: formData.get('imageId') ?? '',
+				file: formData.get('file') ?? null,
+				altText: formData.get('altText') ?? null,
+			},
+		],
 	})
 
 	return redirect(`/users/${params.username}/notes/${params.noteId}`)
@@ -122,7 +128,7 @@ export default function NoteEdit() {
 				method="post"
 				className="flex h-full flex-col gap-y-4 overflow-y-auto overflow-x-hidden px-10 pb-28 pt-12"
 				{...form.props}
-				// 🐨 add the encType="multipart/form-data" here
+				encType="multipart/form-data"
 			>
 				<div className="flex flex-col gap-1">
 					<div>
@@ -147,7 +153,7 @@ export default function NoteEdit() {
 					</div>
 					<div>
 						<Label>Image</Label>
-						{/* 🐨 render the ImageChooser and pass the note's first image as the image prop */}
+						<ImageChooser image={data.note.images[0]} />
 					</div>
 				</div>
 				<ErrorList id={form.errorId} errors={form.errors} />
@@ -169,8 +175,6 @@ export default function NoteEdit() {
 	)
 }
 
-// 💣 you can delete this linter comment once you're using the ImageChooser
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 function ImageChooser({
 	image,
 }: {
@@ -213,7 +217,9 @@ function ImageChooser({
 									➕
 								</div>
 							)}
-							{/* 🐨 if there's an existing image, add a hidden input with a name "imageId" and the value set to the image's id */}
+							{existingImage ? (
+								<input type="hidden" name="imageId" value={image?.id} />
+							) : null}
 							<input
 								id="image-input"
 								aria-label="Image"
@@ -231,9 +237,9 @@ function ImageChooser({
 										setPreviewImage(null)
 									}
 								}}
-								// 🐨 add a name of "file" here:
+								name="file"
 								type="file"
-								// 🐨 add accept="image/*" here so users only upload images
+								accept="image/*"
 							/>
 						</label>
 					</div>
@@ -242,7 +248,7 @@ function ImageChooser({
 					<Label htmlFor="alt-text">Alt Text</Label>
 					<Textarea
 						id="alt-text"
-						// 🐨 add a name of "altText" here
+						name="altText"
 						defaultValue={altText}
 						onChange={e => setAltText(e.currentTarget.value)}
 					/>
