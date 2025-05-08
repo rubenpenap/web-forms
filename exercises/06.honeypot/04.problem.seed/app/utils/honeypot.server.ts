0d09b09e4b799a0514a02ae4e9fc232c7d4fe3cb
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-05-07 23:08:43 -0400

    feat: add encryptionSeed option to honeypot configuration using HONEYPOT_SECRET environment variable

--- exercises/06.honeypot/04.problem.seed/app/utils/honeypot.server.ts
+++ exercises/06.honeypot/04.problem.seed/app/utils/honeypot.server.ts
@@ -2,8 +2,7 @@ import { Honeypot, SpamError } from 'remix-utils/honeypot/server'
 
 export const honeypot = new Honeypot({
 	validFromFieldName: process.env.TESTING ? null : undefined,
-	// 🐨 add an encryptionSeed option to the honeypot constructor
-	// and set it to the HONEYPOT_SECRET environment variable
+	encryptionSeed: process.env.HONEYPOT_SECRET,
 })
 
 export function checkHoneypot(formData: FormData) {
